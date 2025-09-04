3210c1ce525b2d2f69b4de3b240b8ea8c441197b
harikamanne22 <harika2238@gmail.com>
2025-09-04 21:46:02 +0530

    Fix Socket.IO CORS for Netlify deployment

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app=express();
 const server= http.createServer(app);
 const io=new Server(server,{
     cors:{
-        origin:["https://groupdesk.netlify.app/"],
+        origin:["https://groupdesk.netlify.app"],
         methods:["GET","POST"],
         credentials:true
     }
